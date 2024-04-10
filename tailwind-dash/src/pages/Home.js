9406b0f9f6d007167bc59996ebd7bf498010970a
Henok <henokmicrometre@gmail.com>
2024-04-10 08:19:30 +0100

    adding testing

--- tailwind-dash/src/pages/Home.js
+++ tailwind-dash/src/pages/Home.js
@@ -1,12 +1,13 @@
 import ExploreContainer from '../components/ExploreContainer';
 import SideNav from '../components/SideNav';
+import NavBar from '../components/NavBar';
 import "../App.css"
 export default function Home() {
     return (
         <>
             <div className="App bg-white text-gray-600  dark:bg-gray-900 dark:text-gray-400">
                 <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-                    <div className="navbar bg-blue-900 text-gray-100 w-full flex-none md:w-64">
+                    <div className="navbar bg-sky-900 text-gray-100 w-full flex-none md:w-64">
                         <SideNav />
                     </div>
                     <div className=" grow p-6 md:overflow-y-auto md:p-12">
