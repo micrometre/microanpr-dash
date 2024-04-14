8a2d73eaaec3ccab552c57317887640b95e47b85
Henok <henokmicrometre@gmail.com>
2024-04-14 20:24:23 +0100

    sorting dash

--- vite-dash/src/components/DashboardImage.jsx
+++ vite-dash/src/components/DashboardImage.jsx
@@ -10,8 +10,8 @@ export default function DashboardImage() {
                             <button type="button" className=" rounded-xl p-4 text-start outline-none " >
                                 <span className="flex">
                                     <svg className="mt-2 h-6 w-6 flex-shrink-0 fill-neutral-700 hs-tab-active:fill-[#fa5a15] dark:fill-neutral-300 dark:hs-tab-active:fill-[#fb713b] md:h-7 md:w-7" height="48" viewBox="0 -960 960 960" width="48">
-                                        <path d="M510-570v-270h330v270H510ZM120-450v-390h330v390H120Zm390 330v-390h330v390H510Zm-390 0v-270h330v270H120Zm60-390h210v-270H180v270Zm390 330h210v-270H570v270Zm0-450h210v-150H570v150ZM180-180h210v-150H180v150Zm210-330Zm180-120Zm0 180ZM390-330Z" 
-                                        className="">
+                                        <path d="M510-570v-270h330v270H510ZM120-450v-390h330v390H120Zm390 330v-390h330v390H510Zm-390 0v-270h330v270H120Zm60-390h210v-270H180v270Zm390 330h210v-270H570v270Zm0-450h210v-150H570v150ZM180-180h210v-150H180v150Zm210-330Zm180-120Zm0 180ZM390-330Z"
+                                            className="">
                                         </path>
                                     </svg>
                                     <span className="ms-6 grow">
@@ -34,8 +34,8 @@ export default function DashboardImage() {
                                 <button type="button" className=" rounded-xl p-4 text-start outline-none " >
                                     <span className="flex">
                                         <svg className="mt-2 h-6 w-6 flex-shrink-0 fill-neutral-700 hs-tab-active:fill-[#fa5a15] dark:fill-neutral-300 dark:hs-tab-active:fill-[#fb713b] md:h-7 md:w-7" height="48" viewBox="0 -960 960 960" width="48">
-                                            <path d="M510-570v-270h330v270H510ZM120-450v-390h330v390H120Zm390 330v-390h330v390H510Zm-390 0v-270h330v270H120Zm60-390h210v-270H180v270Zm390 330h210v-270H570v270Zm0-450h210v-150H570v150ZM180-180h210v-150H180v150Zm210-330Zm180-120Zm0 180ZM390-330Z" 
-                                            className="">
+                                            <path d="M510-570v-270h330v270H510ZM120-450v-390h330v390H120Zm390 330v-390h330v390H510Zm-390 0v-270h330v270H120Zm60-390h210v-270H180v270Zm390 330h210v-270H570v270Zm0-450h210v-150H570v150ZM180-180h210v-150H180v150Zm210-330Zm180-120Zm0 180ZM390-330Z"
+                                                className="">
                                             </path>
                                         </svg>
                                         <span className="ms-6 grow">
@@ -58,8 +58,8 @@ export default function DashboardImage() {
                                 <button type="button" className=" rounded-xl p-4 text-start outline-none " >
                                     <span className="flex">
                                         <svg className="mt-2 h-6 w-6 flex-shrink-0 fill-neutral-700 hs-tab-active:fill-[#fa5a15] dark:fill-neutral-300 dark:hs-tab-active:fill-[#fb713b] md:h-7 md:w-7" height="48" viewBox="0 -960 960 960" width="48">
-                                            <path d="M510-570v-270h330v270H510ZM120-450v-390h330v390H120Zm390 330v-390h330v390H510Zm-390 0v-270h330v270H120Zm60-390h210v-270H180v270Zm390 330h210v-270H570v270Zm0-450h210v-150H570v150ZM180-180h210v-150H180v150Zm210-330Zm180-120Zm0 180ZM390-330Z" 
-                                            className="">
+                                            <path d="M510-570v-270h330v270H510ZM120-450v-390h330v390H120Zm390 330v-390h330v390H510Zm-390 0v-270h330v270H120Zm60-390h210v-270H180v270Zm390 330h210v-270H570v270Zm0-450h210v-150H570v150ZM180-180h210v-150H180v150Zm210-330Zm180-120Zm0 180ZM390-330Z"
+                                                className="">
                                             </path>
                                         </svg>
                                         <span className="ms-6 grow">
@@ -74,6 +74,34 @@ export default function DashboardImage() {
                             </div>
 
                         </div>
+                    </div>
+                </div>
+                <div class="flex h-auto">
+                    <div class="flex flex-col rounded-xl bg-neutral-50 dark:bg-neutral-700">
+                        <div class="flex-auto p-4 md:p-6">
+                        </div>
+                        <div class="rounded-b-xl bg-neutral-300/30 p-4 dark:bg-neutral-900/30 md:px-7">
+                            <div class="flex items-center">
+                                <div class="ms-3 grow">
+                                </div>
+                            </div>
+                        </div>
+                           <button type="button" className=" rounded-xl p-4 text-start outline-none " >
+                                <span className="flex">
+                                    <svg className="mt-2 h-6 w-6 flex-shrink-0 fill-neutral-700 hs-tab-active:fill-[#fa5a15] dark:fill-neutral-300 dark:hs-tab-active:fill-[#fb713b] md:h-7 md:w-7" height="48" viewBox="0 -960 960 960" width="48">
+                                        <path d="M510-570v-270h330v270H510ZM120-450v-390h330v390H120Zm390 330v-390h330v390H510Zm-390 0v-270h330v270H120Zm60-390h210v-270H180v270Zm390 330h210v-270H570v270Zm0-450h210v-150H570v150ZM180-180h210v-150H180v150Zm210-330Zm180-120Zm0 180ZM390-330Z"
+                                            className="">
+                                        </path>
+                                    </svg>
+                                    <span className="ms-6 grow">
+                                        <span className="block text-lg font-bold text-neutral-800 dark:text-neutral-200 ]">
+                                            <p className="font-bold">
+                                                Dashboard Image component
+                                            </p>
+                                        </span>
+                                    </span>
+                                </span>
+                            </button>
                     </div>
                 </div>
             </div>
--- vite-dash/src/dashboard/sidebar/SidebarItems.jsx
+++ vite-dash/src/dashboard/sidebar/SidebarItems.jsx
@@ -2,11 +2,11 @@ import { data } from "./data"
 import { useLocation, Link } from "react-router-dom"
 
 const style = {
-  title: "font-normal mx-4 text-sm",
+  title: " dark:text-neutral-200 font-normal mx-4 text-sm",
   active:
-    "bg-gradient-to-r border-r-4 border-blue-500 border-r-4 border-blue-500 from-white to-blue-100 text-blue-500",
+    "dark:bg-neutral-800  bg-gradient-to-r border-r-4 border-blue-500 border-r-4 border-blue-500 from-white to-blue-100 text-blue-500",
   link:
-    "duration-200 flex font-thin items-center justify-start my-2 p-4 transition-colors text-gray-500 uppercase w-full lg:hover:text-blue-500"
+    "dark:bg-neutral-800  duration-200 flex font-thin items-center justify-start my-2 p-4 transition-colors text-gray-500 uppercase w-full lg:hover:text-blue-500"
 }
 
 export function SidebarItems() {
