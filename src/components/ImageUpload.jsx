dd93a78aafaca4a0592b73efdffe27e84b6a2cb5
Henok <henokmicrometre@gmail.com>
2024-04-16 22:04:52 +0100

    added upload

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -39,10 +39,6 @@ const MultipleFileUploader = () => {
     <>
 
       <section class="relative not-prose scroll-mt-[72px]">
-        <div class="inset-0 absolute pointer-events-none -z-[1]" aria-hidden="true">
-          <div class="inset-0 absolute">
-          </div>
-        </div>
         <div class="relative mx-auto px-4 lg:py-20 md:px-6 md:py-16 py-12 text-default max-w-5xl">
           <div class="flex flex-col md:flex-row gap-8 md:gap-12">
             <div class="md:basis-1/2 md:py-4 md:self-center">
