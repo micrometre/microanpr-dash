d0408ecdc6a835db95760e2e77f30045ac015d5a
Henok <henokmicrometre@gmail.com>
2024-04-16 23:06:23 +0100

    sorting upload

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -38,9 +38,9 @@ const MultipleFileUploader = () => {
   return (
     <>
 
-      <div class=" dark:bg-neutral-800  bg-white relative mx-auto px-4 lg:py-20 md:px-6 md:py-16 py-12 text-default max-w-5xl">
-        <div class="flex flex-col md:flex-row gap-8 md:gap-12">
-          <div class="md:basis-1/2 md:py-4 md:self-center">
+      <div className=" dark:bg-neutral-800  bg-white relative mx-auto px-4 lg:py-20 md:px-6 md:py-16 py-12 text-default max-w-5xl">
+        <div className="flex flex-col md:flex-row gap-8 md:gap-12">
+          <div className="md:basis-1/2 md:py-4 md:self-center">
             <form action="">
               <div className="relative mt-6">
                 <label htmlFor="file" className="absolute left-0 ml-1 -translate-y-3 bg-white px-1 text-sm duration-100 ease-linear peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:ml-1 peer-focus:-translate-y-3 peer-focus:px-1 peer-focus:text-sm">
@@ -49,17 +49,10 @@ const MultipleFileUploader = () => {
               </div>
               <input type="file" className="mt-10 text-lg font-bold text-neutral-800 dark:text-neutral-200 peer w-full border-b placeholder:text-transparent" accept="image/*" id="files" name="file" onChange={uploadToClient} />
             </form>
-            <div class="mb-8 md:mb-12 md:mx-auto rtl:text-right text-left">
-              <label id="upload-in" htmlFor="file" className="sr-only" >
-                Choose files
-              </label>
-              <input type="file" accept="image/*" id="files" name="file" onChange={uploadToClient} />
-            </div>
-            <div class="">
-              <div class="flex">
-                <div class="flex items-center rtl:mr-0 flex-col mr-4 rtl:ml-4">
-                  <div>
-                    <div class="flex justify-center items-center">
+            <div className="">
+              <div className="flex">
+                <div className="flex items-center rtl:mr-0 flex-col mr-4 rtl:ml-4">
+                    <div className="flex justify-center items-center">
                       <button
                         className="roup inline-flex items-center justify-center gap-x-2 rounded-lg border border-transparent bg-[#fa5a15] px-4 py-3 text-sm font-bold text-neutral-50 outline-none ring-zinc-500 transition duration-300 hover:bg-[#e14d0b] focus-visible:ring active:bg-[#e14d0b] disabled:pointer-events-none disabled:opacity-50 dark:ring-zinc-200 dark:focus:outline-none 2xl:text-base"
                         type="submit"
@@ -69,12 +62,11 @@ const MultipleFileUploader = () => {
                         <Result status={status} />
                       </button>
                     </div>
-                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="relative md:basis-1/2">
+          <div className="relative md:basis-1/2">
             <div className="input-group">
               <Result status={status} />
               <img src={file} />
