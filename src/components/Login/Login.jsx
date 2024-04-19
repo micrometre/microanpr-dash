2b9eca9814fbee66efd8e624b94dddb4f9031c35
Henok <henokmicrometre@gmail.com>
2024-04-19 10:12:56 +0100

    added log

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import './Login.css';
 
 async function loginUser(credentials) {
- return fetch('http://192.168.1.122:5001/login', {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json'
-   },
-   body: JSON.stringify(credentials)
- })
-   .then(data => data.json())
+  return fetch('http://192.168.1.122:5001/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
 }
 
 export default function Login({ setToken }) {
@@ -26,22 +26,25 @@ export default function Login({ setToken }) {
     setToken(token);
   }
 
-  return(
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)} />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)} />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+  return (
+    <div className="login-wrapper bg-neutral-200 selection:bg-yellow-400 selection:text-neutral-700 dark:bg-neutral-800 ">
+      <div className=' text-lg font-bold text-neutral-800 dark:text-neutral-200'>
+
+        <h1>Please Log In</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <p>Username</p>
+            <input type="text" onChange={e => setUserName(e.target.value)} />
+          </label>
+          <label>
+            <p>Password</p>
+            <input type="password" onChange={e => setPassword(e.target.value)} />
+          </label>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
