877eaf3f6682f0dc73b6ff22d0e6421cd8d44abf
Henok <henokmicrometre@gmail.com>
2024-04-21 12:09:37 +0100

    adding ffmpeg

--- src/components/VideoUpload.jsx
+++ src/components/VideoUpload.jsx
@@ -39,8 +39,8 @@ export default function VideoUpload(){
 			const body = new FormData();
 			body.append("file", image);
 			try {
-				//const response = await fetch("http://192.168.1.197:5000/upload_video/", {
-				const response = await fetch("http://192.168.1.122:5000", {
+				//const response = await fetch("http://192.168.1.122:5000", {
+				const response = await fetch("http://192.168.1.197:5000/upload_video/", {
 					method: "POST",
 					body
 				});
