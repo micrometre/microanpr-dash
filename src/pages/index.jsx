c2489f6c5dba378be25ec47ae59cc91edab708bd
Henok <henokmicrometre@gmail.com>
2024-05-23 20:19:24 +0100

    added env

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import LogOut from '../components/LogOut'
+
+console.log(import.meta.env.VITE_APP_TITLE); // "My App"
+console.log(import.meta.env.VITE_APP_URL); // "My App"
+
+
+
 const Home = () => {
   return (
       <LogOut/>
