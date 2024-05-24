b587426865b0f1eee005e3e98873ce7d65d53ae3
Henok <henokmicrometre@gmail.com>
2024-05-24 22:04:07 +0100

    sorting envs

--- src/components/ExportImgaesDb.jsx
+++ src/components/ExportImgaesDb.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+const imgDbUrl = import.meta.env.VITE_APP_IMG_DB_EXPORT
 
 export default function ExportImgaesDb() {
     return (
@@ -9,8 +10,7 @@ export default function ExportImgaesDb() {
                 <div className="flex h-auto">
                     <div className="mt-4 flex items-center rtl:mr-0 flex-col mr-4 rtl:ml-4">
                         <div className="flex justify-center items-center">
-                            <a href="http://192.168.1.122:5000/upload_images/download">
-                            {/*<a href="http://192.168.1.197:5000/upload_images/download">*/} 
+                            <a href={imgDbUrl}>
 
                             <button
                                 className="roup inline-flex items-center justify-center gap-x-2 rounded-lg border border-transparent bg-[#fa5a15] px-4 py-3 text-sm font-bold text-neutral-50 outline-none ring-zinc-500 transition duration-300 hover:bg-[#e14d0b] focus-visible:ring active:bg-[#e14d0b] disabled:pointer-events-none disabled:opacity-50 dark:ring-zinc-200 dark:focus:outline-none 2xl:text-base"
